a066aa37d30b73eae1f29045e43b9fa8a3410a71
Austin Mount <austin@grayskull.adrenlinerush.net>
2024-03-07 08:33:00 -0500

    add script to update running image in deployment

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+echo "Build new image."
+docker build -t adrenlinerush-doc .
+docker image list
+
+echo "Stop running container."
+docker ps
+docker stop adrenlinerush-doc
+docker ps
+
+echo "Remove old container."
+docker rm adrenlinerush-doc
+
+echo "Start new container with new image."
+docker run --name adrenlinerush-doc -p 3000:3000 -d --restart always adrenlinerush-doc
+docker ps
